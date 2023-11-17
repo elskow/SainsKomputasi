1fbbb223ae9ba52d47af1950d5ba99a8b6f0be13
elskow <elskow@outlook.com>
2023-11-17 23:59:39 +0700

    [project 2] jacobi iter

--- Tugas2_Iterasi/jacobi.py
+++ Tugas2_Iterasi/jacobi.py
@@ -1,95 +1,57 @@
 import sympy as sym
 from pprint import pprint
-import numpy as np
+
 
 eq = [
-    "10*x1 - x2 + 2*x3 = 6",
-    "-x1 + 11x2 - x3 + 3*x4 = 25",
-    "2*x1 - x2 + 10*x3 - x4 = -11",
-    "3*x1 - x3 + 8*x4 = 15",
+    "10*x1 - x2 + 2*x3 - 6",
+    "-x1 + 11*x2 - x3 + 3*x4 - 25",
+    "2*x1 - x2 + 10*x3 - x4 + 11",
+    "3*x2 - x3 + 8*x4 - 15",
 ]
-
-
-def clean_eq(eq: str) -> str:
-    return eq.replace("+", "").replace("*", "").replace("/", "").replace(" ", "")
 
 
 class Jacobi:
     def __init__(self, _eq: list) -> None:
-        self.eq = _eq
-        self.__var = []
+        self.eq = []
 
-        # Get the constanta. This method is not good enough
-        # This method only works if the constanta is on the right side of the equation
-        # All code are hardcoded
+        for i in range(len(_eq)):
+            _eq[i] = sym.sympify(_eq[i])
+            _eq[i] = sym.solve(_eq[i], sym.Symbol(f"x{i + 1}"))[0]
 
-        for i in range(len(self.eq)):
-            res_const = self.eq[i].split("=")[1]
+        for i in range(len(_eq) + 1):
+            globals()[f"x{i+1}"] = sym.Symbol(f"x{i+1}")
 
-            var_const = clean_eq(self.eq[i].split("=")[0])
+        for i in range(len(_eq)):
+            self.eq.append(sym.sympify(str(_eq[i])))
 
-            if 0 < i < len(self.eq) - 1:
-                var_const = var_const.split(f"x{i + 1}")[0].split(f"x{i}")[1]
-            elif i == len(self.eq) - 1:
-                var_const = var_const.split(f"x{i}")[1].split(f"x{i + 1}")[0]
-            else:
-                var_const = var_const.split(f"x{i + 1}")[0]
-
-            try:
-                globals()[f"x{i + 1}"] = eval(res_const) / eval(var_const)
-            except ZeroDivisionError:
-                globals()[f"x{i + 1}"] = eval("0")
-            except SyntaxError:
-                globals()[f"x{i + 1}"] = eval(res_const)
-
-            self.__var.append(globals()[f"x{i + 1}"])
+        self.x = {0: ["0"] * len(_eq)}
 
         self.__log_iter = {
             0: {
                 "iter": 0,
+                "xi": self.x[0],
             }
         }
 
-        for i in range(len(self.eq)):
-            self.__log_iter[0][f"x{i + 1}"] = 0
+    def solve(self, n_iter: int) -> list:
+        for i in range(n_iter):
+            x_curr = []
+            for j in range(len(self.eq)):
+                subs_dict = {
+                    sym.Symbol(f"x{k+1}"): float(self.x[i][k])
+                    for k in range(len(self.x[i]))
+                }
 
-    def solve(self, n_iter: int = 3) -> dict:
-        self.__log_iter[1] = {
-            "iter": 1,
-        }
+                x_curr.append(self.eq[j].subs(subs_dict).evalf())
 
-        for i in range(len(self.eq)):
-            self.__log_iter[1][f"x{i + 1}"] = self.__var[i]
+            self.x[i + 1] = x_curr
 
-        for i in range(1, n_iter):
             self.__log_iter[i + 1] = {
                 "iter": i + 1,
+                "xi": self.x[i + 1],
             }
 
-            for j in range(len(self.eq)):
-                for k in range(len(self.eq)):
-                    if k != j:
-                        self.eq[j] = self.eq[j].replace(f"x{k + 1}", str(self.__var[k]))
-
-            for j in range(len(self.eq)):
-                res_const = self.eq[j].split("=")[1]
-                var_const = clean_eq(self.eq[j].split("=")[0])
-                try:
-                    if 0 < j < len(self.eq) - 1:
-                        var_const = var_const.split(f"x{j + 1}")[0].split(f"x{j}")[1]
-                    elif j == len(self.eq) - 1:
-                        var_const = var_const.split(f"x{j}")[1].split(f"x{j + 1}")[0]
-                    else:
-                        var_const = var_const.split(f"x{j + 1}")[0]
-
-                except IndexError:
-                    var_const = var_const.split(f"x{j + 1}")[0]
-
-                self.__var[j] = eval(res_const) / eval(var_const)
-
-                self.__log_iter[i + 1][f"x{j + 1}"] = self.__var[j]
-
-        return self.__var
+        return self.x[n_iter]
 
     def get_log(self) -> dict:
         return self.__log_iter
@@ -97,6 +59,34 @@ class Jacobi:
 
 if __name__ == "__main__":
     jacobi = Jacobi(eq)
-    res = jacobi.solve(n_iter=3)
-
+    pprint(jacobi.solve(5))
     pprint(jacobi.get_log())
+
+# Output:
+# [0.988991301652893, 2.01141472577010, -1.01028590392562, 1.02135051007231]
+# {0: {'iter': 0, 'xi': ['0', '0', '0', '0']},
+#  1: {'iter': 1,
+#      'xi': [0.600000000000000,
+#             2.27272727272727,
+#             -1.10000000000000,
+#             1.87500000000000]},
+#  2: {'iter': 2,
+#      'xi': [1.04727272727273,
+#             1.71590909090909,
+#             -0.805227272727273,
+#             0.885227272727273]},
+#  3: {'iter': 3,
+#      'xi': [0.932636363636364,
+#             2.05330578512397,
+#             -1.04934090909091,
+#             1.13088068181818]},
+#  4: {'iter': 4,
+#      'xi': [1.01519876033058,
+#             1.95369576446281,
+#             -0.968108626033058,
+#             0.973842716942149]},
+#  5: {'iter': 5,
+#      'xi': [0.988991301652893,
+#             2.01141472577010,
+#             -1.01028590392562,
+#             1.02135051007231]}}
